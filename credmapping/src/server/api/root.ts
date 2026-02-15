ff263f5340a5137a14d0e7d3453d29b1f9045dba
Adrian Cosentino <118642079+acozy03@users.noreply.github.com>
2026-02-15 11:59:41 -0500

    removing boiler-plate t3

--- credmapping/src/server/api/root.ts
+++ credmapping/src/server/api/root.ts
@@ -1,23 +1,16 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
  *
- * All routers added in /api/routers should be manually added here.
+ * Add routers in /api/routers and register them here when needed.
  */
-export const appRouter = createTRPCRouter({
-  post: postRouter,
-});
+export const appRouter = createTRPCRouter({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
 
 /**
  * Create a server-side caller for the tRPC API.
- * @example
- * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
- *       ^? Post[]
  */
 export const createCaller = createCallerFactory(appRouter);
